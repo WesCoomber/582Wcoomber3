07706f66b05d874ecc19444b543ba71d8ecd39ae
Wesley Coomber <wesleycoomber@gmail.com>
2016-12-08 01:37:06 -0500

    lateNateWork2 need to parse the 'photo_id_to_business_id.json' file into a dict that has (business_id,photo_id) key,value pairs

--- wcoomberYelp1.py
+++ wcoomberYelp1.py
@@ -5,39 +5,110 @@ import random
 import string
 import json
 
-filename = 'yelp_academic_dataset_business2.json'
+filenameBusiness = 'yelp_academic_dataset_business2.json'
+
+filenamePicJSON = '2016_yelp_dataset_challenge_photos\photo_id_to_business_id2.json'
 
 def get_file_p(name):
-	currentDirPath = os.getcwd()
-	file_path = os.path.join(os.getcwd(), filename)
-	print(file_path)
-	return file_path
+    currentDirPath = os.getcwd()
+    file_path = os.path.join(os.getcwd(), name)
+    #print(file_path)
+    return file_path
 
-fullpath = get_file_p(filename)
+def get_foto_p(name):
+    currentDirPath = os.getcwd()
+    file_path = os.path.join(os.getcwd(), '2016_yelp_dataset_challenge_photos')
+    #print(file_path)
+    return file_path
+
+def getBusinessID(name):
+    for i in range(len(businesses)):
+        if( businesses[i]['name'] == name):
+            #print(businesses[i]['name'], businesses[i]['business_id'])
+            foundRestaurant = businesses[i]['business_id']
+            return foundRestaurant
+
+def insertIntoIDNames(id, name, aDict):
+    if not name in aDict:
+        aDict[id] = [name]
+    else:
+        aDict[id].append(name)
+
+def insertIntoIDPhotoID(id, photoID, aDict):
+    if not name in aDict:
+        aDict[id] = [photoID]
+    else:
+        aDict[id].append(photoID)
+
+
+def insertIntoNamesID(id, name, aDict):
+    if not name in aDict:
+        aDict[name] = [id]
+    else:
+        aDict[name].append(id)
+
+IDNames = {}
+namesID = {}
+IDPhotoID = {}
+
+
+
+
+
+fullpath = get_file_p(filenameBusiness)
+
+fullpathPicJSON = get_file_p(filenamePicJSON)
 i = 0
+
+print('hi',fullpathPicJSON)
+
+
+photos = []
+
+with open(filenamePicJSON, encoding="utf8") as json_data:
+    d = json.load(json_data)
+    print(d)
 
 businesses = []
 
-for line in open(filename, 'r'):
-        businesses.append(json.loads(line))
-
+for line in open(filenameBusiness, 'r'):
+    businesses.append(json.loads(line))
+"""
 data = []
 
-with open(filename) as f:
+with open(filenameBusiness) as f:
     for line in f:
-        data.append(json.loads(line))
+        tempJSON = json.loads(line)
+        data.append(tempJSON)
+        tempID = getBusinessID(businesses[i]['name'])
+        insertIntoIDNames(tempID, businesses[i]['name'],IDNames)
 
 print('helloWorld1')
 print(businesses)
 print('helloWorld2')
 
+print(data)
+print('helloWorld3')
+
+
+"""
 
 print(len(businesses))
 businessesDictionary = dict()
 
 for i in range(len(businesses)):
     print(i, businesses[i]['name'])
-    #print(i, businesses[i])
+    tempID = getBusinessID(businesses[i]['name'])
+    insertIntoIDNames(tempID, businesses[i]['name'], IDNames)
+    insertIntoNamesID(tempID, businesses[i]['name'], namesID)
+
+
+print(IDNames)
+print('helloWorld4')
+print(namesID)
+
+print(IDNames['3ZVKmuK2l7uXPE6lXY4Dbg'])
+print(namesID['Grand View Golf Club'])
 
 print(businesses[0].keys())
 print(businesses[0]['name'])
@@ -46,12 +117,10 @@ print(businesses[0]['categories'][1])
 
 print(businesses[1]['business_id'])
 
-def getBusinessID(name):
-    for i in range(len(businesses)):
-        if( businesses[i]['name'] == 'Emil\'s Lounge':
-            print(i, businesses[i]['name'])
+tempID = getBusinessID('Grand View Golf Club')
+print(tempID)
 
-	return file_path
+
 
 #some test code that searches for dravosburg
 #x = next((item for item in businesses if item["city"] == "Dravosburg"))
