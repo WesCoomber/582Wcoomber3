03f03f6bb471a75b30a7276a4c4848a2240d9ade
Wesley Coomber <wesleycoomber@gmail.com>
2016-12-09 02:35:36 -0500

    significantProgress. Check youtube video for demo of the program.

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # 582Wcoomber3
 
+You give the program a business_ID and/or business 'name' and then if the business has a star rating greater than or equal to the 'loadThreshold' value, it will loadup/open 5 images from the business.
 
-The program requires this folder structure. SEE THE UPLOADED PICTURE 'http://imgur.com/a/smWSi'
+#Set testBusinessID
+#Set testBusinessName
+to whatever you want.
+
+The program requires this exact folder structure. SEE THE UPLOADED PICTURE 'http://imgur.com/a/smWSi'
+
+Tested on windows10. May require changes for linux systems.
--- wcoomberYelp1.py
+++ wcoomberYelp1.py
@@ -7,18 +7,50 @@ import json
 from PIL import Image
 import webbrowser
 
+'''
+//  Created by Wesley Coomber on 11/27/16.
+//  Copyright © 2016 Wesley Coomber. All rights reserved.
+//
+/*
+* Copyright 2016 Wesley Coomber,
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* I am providing the instuctor (Dr. Mosharaf) and the University with the right to build and evaluate the software package for the purpose of determining my grade and my program assessment.
+*
+* Purpose: 582 OS Project, yelp data set speculation/image loading
+*
+* @author Wesley Coomber Wesley.Coomber@umich.edu
+*
+* @version 11/27/16.
+'''
+
 filenameBusiness = 'yelp_academic_dataset_business.json'
 
 filenamePicJSON = '2016_yelp_dataset_challenge_photos\photo_id_to_business_id.json'
 
 #Change this testBusinessID to the exact string Business_ID of the business that you want to open photos for
-testBusinessID = '52oCXlQmP2kXt53xpl9a3w'
+#testBusinessID = '52oCXlQmP2kXt53xpl9a3w'
+testBusinessID = 'KayYbHCt-RkbGcPdGOThNg'
 #Change this testBusinessName to the exact string name of the business that you want to open photos for
-testBusinessName = 'Hazelrock Coffee + Sweets'
+#EG. 'Flamingo Las Vegas Hotel & Casino' and 'NGJDjdiDJHmN2xxU7KauuA'
+#testBusinessName = 'Hazelrock Coffee + Sweets'
+testBusinessName = 'Flamingo Las Vegas Hotel & Casino'
 
 
 #Star rating of the restaurants must be equal to or greater than this value to get their images opened.
-loadThreshold = 4
+loadThreshold = 1.9
+
+maxNumberOfPicsToLoad = 5
 
 def get_file_p(name):
     currentDirPath = os.getcwd()
@@ -64,6 +96,23 @@ def insertIntoNamesID(id, name, aDict):
     else:
         aDict[name].append(id)
 
+def showPhotosOfBusinessID(businessID):
+    if ((businessIDStars[businessID][0]) >= loadThreshold):
+        if(len(businessIDPhotoID[businessID]) <= maxNumberOfPicsToLoad):
+            print(businessIDStars[businessID][0], 'rating greater than or equal to loadThreshold value of ', loadThreshold)
+            for i in range(len(businessIDPhotoID[businessID])):
+                tempImagePath = get_foto_p((businessIDPhotoID[businessID])[i]+'.jpg')
+                webbrowser.open(tempImagePath)
+                #print(i,tempImagePath)
+        else:
+            print(businessIDStars[businessID][0], 'rating greater than or equal to loadThreshold value of ', loadThreshold)
+            for i in range(maxNumberOfPicsToLoad):
+                tempImagePath = get_foto_p((businessIDPhotoID[businessID])[i] + '.jpg')
+                webbrowser.open(tempImagePath)
+                # print(i,tempImagePath)
+    else:
+        print(businessIDStars[businessID][0], 'rating less than loadThreshold value of ', loadThreshold, 'Program will not open the photos for the business!')
+
 ''' work in progress function, to do if more time--
 def getPhotosForBusiness(businessID, aDict):
     if not name in aDict:
@@ -72,10 +121,14 @@ def getPhotosForBusiness(businessID, aDict):
         aDict[name].append(id)
 '''
 
-
+#dictionaries (key,value)
+#IDNames (business_id, name)
 IDNames = {}
+#namesID (name, business_id)
 namesID = {}
+#businessIDPhotoID (business_id, photo_id)
 businessIDPhotoID = {}
+#businessIDStars (business_id, stars)
 businessIDStars = {}
 
 
@@ -136,7 +189,7 @@ print(businesses[0]['categories'][1])
 print(businesses[1]['business_id'])
 
 tempID = getBusinessID('Grand View Golf Club')
-print(tempID)'''
+print(tempID)
 
 
 #print(len(businessIDPhotoID))
@@ -153,24 +206,19 @@ print(tempID)'''
 
 #testing webbrowser.open function for opening exact file path
 #webbrowser.open('C:\\Users\\wesle_000\\Documents\\582Wcoomber3\\2016_yelp_dataset_challenge_photos\\tkSP0R3EFRbkHGBkKiva_w.jpg')
+print(IDNames['NGJDjdiDJHmN2xxU7KauuA'])
+'''
 
 
 
-def showPhotosOfBusinessID(businessID):
-    if ((businessIDStars[businessID][0]) >= loadThreshold):
-        print(businessIDStars[businessID][0], ' greater than or equal to loadThreshold value of ', loadThreshold)
-        for i in range(len(businessIDPhotoID[businessID])):
-            tempImagePath = get_foto_p((businessIDPhotoID[businessID])[i]+'.jpg')
-            webbrowser.open(tempImagePath)
 
-            #print(i,tempImagePath)
-    else:
-        print(businessIDStars[businessID][0], ' less than loadThreshold value of ', loadThreshold, 'Program will not open the photos for the business!')
 
 
 #Use the business ID as a key into 'businessIDPhotoID' to get all the photo_id's associated with the business
 #print('gettingAllBusinessPhotosTest1', businessIDPhotoID[testBusinessID])
 #print('hi3', businessIDPhotoID[namesID['Grand View Golf Club'][0]])
+
+
 
 
 #This block of code is different ways to open/load all the photos of the business 1)by ID 2)by name using namesID dict
